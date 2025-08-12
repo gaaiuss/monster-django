9068f7106e67b2022af66bb4e26b6c5d67b9082a
gaaiuss <caio.gui.castro@gmail.com>
2025-08-12 10:31:32 -0300

    moved views to right local

--- project/urls.py
+++ project/urls.py
@@ -15,22 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-def home(request):
-    print('HOME')
-    return HttpResponse('HOME')
-
-
-def blog(request):
-    print('Show no mercy')
-    return HttpResponse('Welcome to your doom')
-
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('home.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', blog),
-    path('', home),
 ]
