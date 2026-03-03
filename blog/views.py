33fa8fdb00dc14a816fd43955f4d59ec28387a08
gaaiuss <caio.gui.castro@gmail.com>
2026-03-03 14:08:56 -0300

    add: uv init

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
-
-
-# Create your views here.
-def blog(request):
-    print('BLOG')
-    return HttpResponse('May you death serve all beings...')
