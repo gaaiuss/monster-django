dd31ebb203aa0db232b3cc747265c0a03fa29f24
gaaiuss <caio.gui.castro@gmail.com>
2026-03-05 10:43:03 -0300

    add: context var on views

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,8 +3,22 @@ from django.shortcuts import render
 
 
 def blog(request: HttpRequest) -> HttpResponse:
-    return render(request=request, template_name="blog/index.html")
+    print("Blog")
+    context = {"text": "We arrived at blog", "title": "Blog - "}
+
+    return render(
+        request=request,
+        template_name="blog/index.html",
+        context=context,
+    )
 
 
 def example(request: HttpRequest) -> HttpResponse:
-    return render(request=request, template_name="blog/example.html")
+    print("Example")
+    context = {"text": "We arrived at blog example", "title": "Example - "}
+
+    return render(
+        request=request,
+        template_name="blog/example.html",
+        context=context,
+    )
--- src/home/views.py
+++ src/home/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request: HttpRequest) -> HttpResponse:
     print("Home")
-    return render(request=request, template_name="home/index.html")
+
+    context = {"text": "We arrived at home"}
+
+    return render(
+        request=request,
+        template_name="home/index.html",
+        context=context,
+    )
