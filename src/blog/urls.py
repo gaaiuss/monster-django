c555e3ebc0051f6748a29815ad3ea5f491e0e473
gaaiuss <caio.gui.castro@gmail.com>
2026-03-05 11:27:57 -0300

    add: post block partials

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from blog import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path("", views.blog),
-    path("example/", views.example),
+    path("", views.blog, name="home"),
+    path("example/", views.example, name="example"),
 ]
--- src/home/urls.py
+++ src/home/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from home import views
 
-urlpatterns = [path("", views.home)]
+app_name = "home"
+
+urlpatterns = [path("", views.home, name="home")]
